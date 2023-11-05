d9aebf623f49570ba3ff52fc3e2e8f3a1a795d96
Justin Chan <75227648+yasteen@users.noreply.github.com>
2023-11-04 23:48:07 -0700

    Update lazy.lua

--- lua/yasteen/lazy.lua
+++ lua/yasteen/lazy.lua
@@ -21,7 +21,6 @@ require("lazy").setup({
     -- Navigation
     { "nvim-telescope/telescope.nvim", tag = "0.1.4",
         dependencies = { "nvim-lua/plenary.nvim" } },
-    { "theprimeagen/harpoon", dependencies = { "nvim-lua/plenary.nvim" } },
 
     { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 
